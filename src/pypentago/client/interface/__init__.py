7790ab48549d4088f80548bf8d4643ee16fe195d
Florian Mayer <flormayer@aim.com>
2008-11-08 00:23:49 +0100

    Remove TODO.

--- src/pypentago/client/interface/__init__.py
+++ src/pypentago/client/interface/__init__.py
@@ -17,10 +17,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# TODO:
-# Notificate about position(position flash)
-# Visualize rotate(arrows popup + one flash)
-
 
 import sys
 import warnings
