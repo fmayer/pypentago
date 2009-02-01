d381bfe84719146b41c7ff1c91887a8d51f0027f
Florian Mayer <flormayer@aim.com>
2009-02-01 20:24:49 +0100

    Remove superfluous function.

--- src/unittests/main.py
+++ src/unittests/main.py
@@ -28,11 +28,16 @@ import sqlalchemy
 
 from itertools import chain
 from pypentago import core
+from qtest import QTestRunner, QTestResult, QtGui, QBGTestRunner, QBGTestResult
+
 
 MODULES = ['core_test', 'pgn_test', 'actions_test', 'crypto_test', 
            'server_test', 'elo_test', 'db_test']
 
 if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+    result = QBGTestResult()
+    result.show()
     for elem in sys.argv[1:]:
         if elem.strip() in MODULES:
             MODULES.remove(elem)
@@ -47,10 +52,13 @@ if __name__ == '__main__':
     print 
     test_cases = chain(*[unittest.findTestCases(mod) for mod in mod])
     suite = unittest.TestSuite(test_cases)
-    display = unittest.TextTestRunner()
+    display = QBGTestRunner(result)
     result = display.run(suite)
-    if result.errors == 0 and result.failures == 0:
-        ret = 0
-    else:
-        ret = 1
-    sys.exit(ret)
+    #if result.errors == 0 and result.failures == 0:
+    #    ret = 0
+    #else:
+    #    ret = 1
+    #print 'foo'
+    app.exec_()
+    #print 'bar'
+    #sys.exit(ret)
