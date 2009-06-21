a669084034b386929cb90868b651561818c83fc7
Florian Mayer <flormayer@aim.com>
2009-06-21 18:02:59 +0000

    I changed my mind, stonebg.png only has 1/4 of each side too.

--- src/pypentago/client/interface/game.py
+++ src/pypentago/client/interface/game.py
@@ -268,7 +268,7 @@ class Quadrant(QtGui.QLabel, BackendQuadrant):
                 if x_c == b_col and y_c == b_row:
                     paint.setOpacity(self.blink_stone.value)
                 stone_value = self.field[y_c][x_c]
-                paint.drawImage(x_p, y_p, self.stone_background)
+                paint.drawImage(x_p+d_size, y_p+d_size, self.stone_background)
                 paint.drawImage(x_p+d_size, y_p+d_size, imgs[stone_value])
                 if x_c == b_col and y_c == b_row:
                     paint.setOpacity(1)
